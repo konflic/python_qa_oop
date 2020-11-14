8cb0d29f449f59cd313313c74a50a48cb705387c
samuraii <konflic@mail.ru>
2020-11-14 10:53:25 +0300

    Версия 1.1

--- source/Hero.py
+++ source/Hero.py
@@ -1,35 +1,49 @@
 class Hero:
-    __health = None
-    __defend = None
-    __healing = None
-    __power = None
-    __name = None
+    __HERO_COUNT = 0
 
     def __init__(self, defend, healing, power, name):
         if defend + healing + power > 100:
-            raise AttributeError("Такого героя создать нельзя, сумма всех навыков должна быть ровно 100")
+            raise AttributeError("Sum of defend, healing and power skill can not be > 100")
         self.__health = 100
         self.__defend = defend
         self.__healing = healing
         self.__power = power
         self.__name = name
-        print(f"Hero {name} with {defend} {healing} {power} created!")
+        self.__increase_count()
+        print(f"Hero '{name}' with {defend} {healing} {power} created!")
 
     def __str__(self):
+        return "{}:{}hp".format(self.__name, self.__health)
+
+    def __del__(self):
+        self.__HERO_COUNT -= 1
+        print("Hero destroyed! Only {} Hero left!".format(self.__HERO_COUNT))
+
+    @property
+    def name(self):
         return self.__name
 
     def hit(self, other_hero):
-        other_hero.take_hit(self)
+        if not isinstance(other_hero, Hero):
+            raise Exception("You can hit only Hero!")
+        other_hero.__take_hit(self)
 
     def heal(self):
         if self.__health == 100:
-            return "Health is full"
+            return "Hero health is full"
         else:
             res = self.__health + self.__healing
             self.__health = 100 if res > 100 else res
 
-    def take_hit(self, other_hero):
-        self.__health = self.__health - (other_hero.__power - self.__defend)
+    def __take_hit(self, other_hero):
+        damage = other_hero.__power - self.__defend
+        self.__health = self.__health - damage
+        print("Hero '{}' hit hero '{}' and made {} damage!".format(other_hero.name, self.__name, damage))
+
+    def __increase_count(self):
+        self.__HERO_COUNT += 1
+        print("There are {} Hero!".format(self.__HERO_COUNT))
 
     def get_health(self):
+        print("Hero '{}' health is {}".format(self.__name, self.__health))
         return self.__health
--- tests/test_battle.py
+++ tests/test_battle.py
@@ -3,18 +3,24 @@ import pytest
 from source.Hero import Hero
 
 
-def test_hero():
+def test_hero_creation():
     with pytest.raises(AttributeError):
-        hero1 = Hero(defend=30, healing=30, power=70, name="Ninja")
+        Hero(defend=30, healing=30, power=70, name="Ninja")
+
+
+def test_hero_hit_negative():
+    class Car: pass
+
+    with pytest.raises(Exception):
+        hero1 = Hero(defend=30, healing=30, power=40, name="Ninja")
+        hero1.hit(Car())
 
 
 def test_hit():
     hero1 = Hero(defend=30, healing=30, power=40, name="Ninja")
     hero2 = Hero(defend=30, healing=40, power=30, name="Optimus")
-
     hero1.hit(hero2)
     assert hero2.get_health() == 90
-
     hero2.hit(hero1)
     assert hero1.get_health() == 100
 
@@ -22,9 +28,7 @@ def test_hit():
 def test_heal():
     hero1 = Hero(defend=30, healing=30, power=40, name="Ninja")
     hero2 = Hero(defend=30, healing=40, power=30, name="Optimus")
-
     hero1.hit(hero2)
     assert hero2.get_health() == 90
-
     hero2.heal()
     assert hero2.get_health() == 100
